196ddc9b99dc9e0d25df5202fbfa662a68617752
Ayush Tiwari <iamayushtripathi.09@gmail.com>
2026-06-04 17:41:47 +0530

    Implement twoSum function using unordered_map

--- Week_1/Day_1/1-two-sum/1-two-sum.cpp
+++ Week_1/Day_1/1-two-sum/1-two-sum.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            int ans = target - nums[i];
+            // Check if the complement exists in the map
+            if(mp.find(ans) != mp.end()) {
+                return {mp[ans], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
